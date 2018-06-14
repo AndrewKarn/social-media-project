03e0a330060050ead590cac19247d8bf742f8b71
Zoe Robertson <zoe.robertson@getalma.com>
2018-06-13 17:58:10 -0700

    updated gitignore

---
+++ src/library/refactored/shared/Key.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zoerobertson
+ * Date: 4/19/18
+ * Time: 12:42 PM
+ */
+
+namespace Utility;
+
+
+class Key
+{
+ const MAIL_GUN_API_KEY = 'key-a75119c3734c1a3ee19f2e3fdbefda92';
+ const JWT_SECRET = 'RGJMN0xnbGdxb1Y0TlBFSUFicUY=';
+}
